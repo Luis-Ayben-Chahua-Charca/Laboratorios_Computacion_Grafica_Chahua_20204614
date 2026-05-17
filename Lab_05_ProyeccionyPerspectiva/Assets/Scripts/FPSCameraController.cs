5b7f9e753b7dcc8da5e5a88161a1102b606c5afa
Luis-Ayben-Chahua-Charca <lchahua@unsa.edu.pe>
2026-05-16 22:10:07 -0500

    Feat: Added First Person Camera Lab05 v1.1.0

---
+++ Lab_05_ProyeccionyPerspectiva/Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class FPSCameraController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform playerBody;
+    [SerializeField] private Transform cameraPivot;
+
+    [Header("Mouse Settings")]
+    [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private float maxVerticalAngle = 80f;
+
+    private float verticalRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMouseLook();
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        // Rotate the player body horizontally
+        playerBody.Rotate(Vector3.up * mouseX);
+        // Rotate the camera vertically
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -maxVerticalAngle, maxVerticalAngle);
+        cameraPivot.localRotation =
+           Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+}
--- Lab_05_ProyeccionyPerspectiva/Assets/Scripts/PlayerController.cs
+++ Lab_05_ProyeccionyPerspectiva/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float gravity = 9.81f;
 
     [Header ("Other Inputs")]
+    [SerializeField] private string verticalMoveInput = "Vertical";
+    [SerializeField] private string horizontalMoveInput = "Horizontal";
     [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
@@ -33,7 +35,34 @@ public class PlayerController : MonoBehaviour
     void HandleMovement()
     {
         float speedMultiplier = Input.GetKey(sprintKey) ? runMultiplier : 1f;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis(horizontalMoveInput);
+        float verticalInput = Input.GetAxis(verticalMoveInput);
+
+        float currentSpeed = walkSpeed * speedMultiplier;
+
+        Vector3 movement = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        currentMovement.x = movement.x * currentSpeed;
+        currentMovement.z = movement.z * currentSpeed;
+
+        handleGravityAndJumping();
+
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void handleGravityAndJumping()
+    {
+        if (characterController.isGrounded)
+        {
+            currentMovement.y = -0.5f; 
+            if (Input.GetKeyDown(jumpKey))
+            {
+                currentMovement.y = jumpForce;
+            }
+        }
+        else
+        {
+            currentMovement.y -= gravity * Time.deltaTime; // Apply gravity when in the air
+        }
     }
 }
